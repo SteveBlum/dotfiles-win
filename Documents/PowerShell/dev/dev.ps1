131f072a3628d3a8e7c4140f9b52d96040ab045b
Steve Blum <steve.blum@live.de>
2026-01-20 00:00:36 +0100

    feat: improved volume handling of development environment

--- Documents/PowerShell/dev/dev.ps1
+++ Documents/PowerShell/dev/dev.ps1
@@ -5,16 +5,20 @@ if ([string]::IsNullOrEmpty($name)) {
     exit 1
 }
 
-$devInitCalled = $false
-if (-not (Test-Path "$PSScriptRoot/$name.env")) {
-    . "$PSScriptRoot/dev-init.ps1" -name $name
-    $devInitCalled = $true
-}
+if (Test-Path -Path $PWD\$name) {
+    $env:WORKSPACE_PATH="$PWD\$name"; docker compose --env-file "$PSScriptRoot/.env.last" -f "$PSScriptRoot/docker-compose.yml" -f "$PSScriptRoot/docker-compose.path.yml" up -d
+} else {
+  $devInitCalled = $false
+  if (-not (Test-Path "$PSScriptRoot/$name.env")) {
+      . "$PSScriptRoot/dev-init.ps1" -name $name
+      $devInitCalled = $true
+  }
 
-# Check if docker compose services are already running
-$runningServices = docker compose --env-file "$PSScriptRoot/$name.env" -f "$PSScriptRoot/docker-compose.dev.yml" ps --status running -q
+  # Check if docker compose services are already running
+  $env:WORKSPACE_VOLUME="$name"; $runningServices = docker compose --env-file "$PSScriptRoot/$name.env" -f "$PSScriptRoot/docker-compose.yml" -f "$PSScriptRoot/docker-compose.volume.yml" ps --status running -q
 
-if ($devInitCalled -or ([string]::IsNullOrEmpty($runningServices))) {
-    docker compose --env-file "$PSScriptRoot/$name.env" -f "$PSScriptRoot/docker-compose.dev.yml" up -d
+  if ($devInitCalled -or ([string]::IsNullOrEmpty($runningServices))) {
+    $env:WORKSPACE_VOLUME="$name"; docker compose --env-file   "$PSScriptRoot/$name.env" -f "$PSScriptRoot/docker-compose.yml" -f "$PSScriptRoot/docker-compose.volume.yml" up -d
+  }
 }
 docker attach dev
